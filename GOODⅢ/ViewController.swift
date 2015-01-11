b519e47ab827d05719d0cd91d1d90c523fc33bef
QiMeng <616684889@qq.com>
2015-01-11 08:34:02 +0800

    ..

--- GOODⅢ/ViewController.swift
+++ GOODⅢ/ViewController.swift
@@ -4,12 +4,12 @@
 //
 //  Created by QiMENG on 1/6/15.
 //  Copyright (c) 2015 QiMENG. All rights reserved.
-//
+//  ....
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
